4ad2bfdc0374bb5ac738ea45be2ea0ba1b8ca162
Yuqing Wang <bibolikepeaches@gmail.com>
2023-06-09 01:07:51 +0100

    pushing locally tested code
    Relates #12

--- tfl.js
+++ tfl.js
@@ -1,75 +1,88 @@
-// require('dotenv').config()
-
 const stationId = '940GZZLUFPK'; // Finsbury Park station ID for Victoria Line
-// const appKey = process.env.API_KEY; // Retrieve the API key from the environment variable
 const appKey = "trustno1";
 
 function getDepartureTimes() {
   const url = `https://api.tfl.gov.uk/Line/victoria/Arrivals/${stationId}`;
-
   fetch(url, {
     method: 'GET',
     headers: {
       'app_key': appKey
     }
   })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        console.log(response.status);
-        throw new Error('Error retrieving departure times');
-      }
-    })
-    .then(departures => {
-      console.log(departures)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      console.log(response.status);
+      throw new Error('Error retrieving departure times');
+    }
+  })
+  .then(departures => {
+
+    if (departures.length === 0) {
       const departureInfoArray = [];
-      //write a for loop the get the next three departures
-      const platform = departures[0].platformName;
-      const currentTime = departures[0].timestamp;
-      const currentTimeBST = new Date(currentTime).toLocaleTimeString('en-GB', { timeZone: 'Europe/London' });
+      console.log('No departures')
+      let departureInfo = {
+        platform: 'Sorry, no train info available at the moment.'
+      };
 
-      // console.log('Current Time:', currentTimeBST);
-
-      for (let i = 0; i < departures.length; i++) {
-        const departure = departures[i];
-        const destination = departure.destinationName;
-        const timeToStation = departure.timeToStation;
-        const timeToStationMinutes = Math.floor(timeToStation / 60);
-
-        let departureInfo = {
-          departureNumber: i + 1,
-          platform: platform,
-          destination: destination,
-          timeToStationMinutes: timeToStationMinutes
-        };
-
-        departureInfoArray.push(departureInfo); // Append departureInfo object to the array
-
-        // console.log('Departure', i + 1);
-        // console.log('Platform:', platform);
-        // console.log('Destination:', destination);
-        // console.log('Time to Station (minutes):', timeToStationMinutes);
-        // console.log('--------------------------');
-
-
-      }
-      // console.log(departureInfoArray);
+      departureInfoArray.push(departureInfo); // Append departureInfo object to the array
       localStorage.setItem('myStorage', JSON.stringify(departureInfoArray));
-      // return departureInfoArray;
-    })
-    .catch(error => {
-      console.error(error);
-    });
+      return;}
 
+    console.log(departures)
+    // const currentTime = departures[0].timestamp;
+    // const currentTimeBST = new Date(currentTime).toLocaleTimeString('en-GB', { timeZone: 'Europe/London' });
+    // console.log('Current Time:', currentTimeBST);
 
+    retrieveDepartureTimes(departures)
+  })
+  .catch(error => {
+    console.error(error);
+  });
 }
+getDepartureTimes()
+
+function retrieveDepartureTimes(departures) {
+
+  const departureInfoArray = [];
+    //write a for loop the get the next three departures
+  const platform = departures[0].platformName;
+  let num = departures.length;
+
+  // only get the next 5 departures max
+  if (num> 5) {
+    num = 5;
+  }
+
+  for (let i = 0; i < num; i++) {
+    const departure = departures[i];
+    const destination = departure.destinationName;
+    const timeToStation = departure.timeToStation;
+    const timeToStationMinutes = Math.floor(timeToStation / 60);
+    const expectedArrival = departure.expectedArrival;
+    const adjustedArrival = new Date(expectedArrival).toLocaleTimeString('en-GB', { timeZone: 'Europe/London' })
+    console.log(adjustedArrival)
+
+    let departureInfo = {
+      departureNumber: i + 1,
+      platform: platform,
+      destination: destination,
+      timeToStationMinutes: timeToStationMinutes
+    };
+
+    departureInfoArray.push(departureInfo); // Append departureInfo object to the array
+    localStorage.setItem('myStorage', JSON.stringify(departureInfoArray));
+}}
 
 const departureInfoArray = JSON.parse(localStorage.getItem('myStorage'));
 
 console.log(departureInfoArray);
 
 const table = document.querySelector('table');
+
+if (departureInfoArray) {
+
 for (const item of departureInfoArray) {
   const row = document.createElement('tr');
   for (const key in item) {
@@ -78,4 +91,4 @@ for (const item of departureInfoArray) {
     row.appendChild(cell);
   }
   table.appendChild(row);
-}
+}}
