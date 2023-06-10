a7d19428463085f18a52f10551b7bc6a1bff6b72
Yuqing Wang <bibolikepeaches@gmail.com>
2023-06-10 18:52:27 +0100

    dispaly max 3 departures for each direction
    Relates #25

--- js/tfl.js
+++ js/tfl.js
@@ -1,106 +1,183 @@
-const stationId = '940GZZLUFPK'; // Finsbury Park station ID for Victoria Line
-
-const table = document.querySelector('table');
-const inboundTable = document.querySelector('#inbound-table');
-const outboundTable = document.querySelector('#outbound-table');
+const table = document.querySelector("table");
+const inboundTable = document.querySelector("#inbound-table");
+const outboundTable = document.querySelector("#outbound-table");
 
 function getDepartureTimes() {
   // query the tfl proxy we made
-  const url = 'https://tfl-irbcjbnqca-ew.a.run.app/search'
-  fetch (url)
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      console.log(response.status);
-      throw new Error('Error retrieving departure times');
-    }
-  })
-  .then(departures => {
-    // display error message if no train found (not tested yet)
-    if (departures.length === 0) {
-      console.log('No departures')
+  const url = "https://tfl-irbcjbnqca-ew.a.run.app/search";
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.log(response.status);
+        throw new Error("Error retrieving departure times");
+      }
+    })
+    .then((allDepartures) => {
+      // only keep departures from Finsbury Park
+      const departures = allDepartures.filter((departure) =>
+        departure.stationName.includes("Finsbury Park")
+      );
 
-      const row = document.createElement('tr');
-      const cell = document.createElement('td');
-      cell.textContent = 'Sorry, no train info available at the moment.';
-      row.appendChild(cell);
-      table.appendChild(row);
-      return;
-    }
+      // display error message if no train found (not tested yet)
+      if (departures.length === 0) {
+        console.log("No departures");
 
-    const currentTime = departures[0].timestamp;
-    const currentTimeBST = new Date(currentTime).toLocaleTimeString('en-GB', { timeZone: 'Europe/London' });
-    console.log('Current Time:', currentTimeBST);
+        const row = document.createElement("tr");
+        const cell = document.createElement("td");
+        cell.textContent = "Sorry, no train info available at the moment.";
+        row.appendChild(cell);
+        table.appendChild(row);
+        return;
+      }
 
-    retrieveDepartureTimes(departures)
-  })
-  .catch(error => {
-    console.error(error);
-  });
+      const currentTime = departures[0].timestamp;
+      const currentTimeBST = new Date(currentTime).toLocaleTimeString("en-GB", {
+        timeZone: "Europe/London",
+      });
+      console.log("Current Time:", currentTimeBST);
+
+      retrieveDepartureTimes(departures);
+
+      const departureInfoArray = JSON.parse(localStorage.getItem("myStorage"));
+
+      const [northboundNew, southboundNew] = sortDepartures(departureInfoArray);
+
+      populateTable(northboundNew, "outbound");
+      populateTable(southboundNew, "inbound");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
-getDepartureTimes()
 
 function retrieveDepartureTimes(departures) {
+  /*
+  given an object of departures, return an array of objects
+  with the following properties:
+  departureNumber, platform, destination, timeToStationMinutes,
+  arriveIn, ArrivalTime, direction
+  */
 
   const departureInfoArray = [];
 
-  // only get the next 5 departures max
-  let num = departures.length;
-  if (num > 5) {
-    num = 5;
-  }
-
-  for (let i = 0; i < num; i++) {
+  for (let i = 0; i < departures.length; i++) {
     const departure = departures[i];
     const platform = departure.platformName
-      .replace("Northbound - Platform ", '')
-      .replace("Southbound - Platform ", '');
-    const destination = departure.destinationName.replace("Underground Station", '');
+      .replace("Northbound - Platform ", "")
+      .replace("Southbound - Platform ", "");
+    let destination = departure.destinationName.replace(
+      "Underground Station",
+      ""
+    );
     const timeToStation = departure.timeToStation;
     const timeToStationMinutes = Math.floor(timeToStation / 60);
     const expectedArrival = departure.expectedArrival;
-    const adjustedArrival = new Date(expectedArrival).toLocaleTimeString('en-GB', { timeZone: 'Europe/London' })
-    const direction = departure.direction;
+    // adjust for british summer time
+    const adjustedArrival = new Date(expectedArrival).toLocaleTimeString(
+      "en-GB",
+      { timeZone: "Europe/London" }
+    );
+    let direction = departure.direction;
+
+    // removing white space from destination and filling in missing direction
+    if (destination === "Walthamstow Central ") {
+      destination = "Walthamstow Central";
+      direction = "outbound";
+    }
+    if (destination === "Brixton ") {
+      destination = "Brixton";
+      direction = "inbound";
+    }
 
     let departureInfo = {
       departureNumber: i + 1,
       platform: platform,
       destination: destination,
       timeToStationMinutes: timeToStationMinutes,
-      arriveIn: timeToStationMinutes + ' mins',
+      arriveIn: timeToStationMinutes + " mins",
       ArrivalTime: adjustedArrival,
-      direction: direction
+      direction: direction,
     };
 
     departureInfoArray.push(departureInfo); // Append departureInfo object to the array
-    localStorage.setItem('myStorage', JSON.stringify(departureInfoArray));
-}}
+    localStorage.setItem("myStorage", JSON.stringify(departureInfoArray));
+  }
+}
 
-const departureInfoArray = JSON.parse(localStorage.getItem('myStorage'))
-.sort(function(a, b) {
-  console.log(a.timeToStationMinutes - b.timeToStationMinutes);
-  return a.timeToStationMinutes - b.timeToStationMinutes;
-});
-console.log(departureInfoArray);
+function sortDepartures(departureInfoArray) {
+  //sort departureInfoArray by timeToStationMinutes
+  const sortedDepartures = departureInfoArray.sort(
+    (a, b) => a.timeToStationMinutes - b.timeToStationMinutes
+  );
 
+  console.log("sorted departures", sortedDepartures);
 
-if (departureInfoArray) {
+  /* separate departureInfoArray by direction, as two arrays
+  and another one for no direction */
 
-for (const item of departureInfoArray) {
+  const northbound = sortedDepartures.filter(
+    (departure) => departure.direction === "outbound"
+  );
+  const southbound = sortedDepartures.filter(
+    (departure) => departure.direction === "inbound"
+  );
+  const noDirection = sortedDepartures.filter(
+    (departure) => departure.direction === ""
+  );
 
-  const row = document.createElement('tr');
-  for (const key in item) {
-    if (key!=='direction' && key!=='departureNumber' && key!=='timeToStationMinutes') {
-    const cell = document.createElement('td');
-    cell.textContent = item[key];
-    row.appendChild(cell);
+  // only keep the first instance of the same timeToStationMinutes
+  removeDuplicates(northbound);
+  removeDuplicates(southbound);
+
+  //keep maximum of 3 departures per direction
+  const northboundNew = northbound.slice(0, 3);
+  const southboundNew = southbound.slice(0, 3);
+  return [northboundNew, southboundNew];
+}
+
+function removeDuplicates(array) {
+  // remove duplicate departures with the same timeToStationMinutes and platform
+  for (let i = 0; i < array.length - 1; i++) {
+    const departure = array[i];
+    const timeToStation = departure.timeToStationMinutes;
+    const nextDeparture = array[i + 1];
+    const nextTimeToStation = nextDeparture.timeToStationMinutes;
+    const platform = departure.platform;
+    if (
+      timeToStation === nextTimeToStation &&
+      platform === nextDeparture.platform
+    ) {
+      array.splice(i + 1, 1);
+      i--; // Adjust the index to revisit the current position
     }
   }
+}
 
-  if (item.direction === 'inbound') {
-    inboundTable.appendChild(row);
-  } else if (item.direction === 'outbound') {
-    outboundTable.appendChild(row);
+function populateTable(array, direction) {
+  // function to loop through array and populate table accordingly
+  for (const item of array) {
+    const row = document.createElement("tr");
+    for (const key in item) {
+      if (
+        !["direction", "departureNumber", "timeToStationMinutes"].includes(key)
+      ) {
+        const cell = document.createElement("td");
+        cell.textContent = item[key];
+        row.appendChild(cell);
+      }
+    }
+
+    if (direction === "inbound") {
+      inboundTable.appendChild(row);
+    }
+    if (direction === "outbound") {
+      outboundTable.appendChild(row);
+    }
+
+    console.log(direction + " information added to table");
   }
-}}
+}
+
+getDepartureTimes();
