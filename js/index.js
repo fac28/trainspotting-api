f821723d4b4eed3863debc5ccdd245bba7be17e4
Carlos Buitrago <letscatchupagain@gmail.com>
2023-06-13 17:50:23 +0100

    implemented fetch

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
+const body = document.querySelector('.body');
+const author = document.querySelector('.background-pic__author')
 
+const getImage = async () => {
+    const response = await fetch('https://picsum.photos/id/348/info');
+    const data = await response.json();
+    return data;
+}
+
+const init = async () => {
+    try {
+        const result = await getImage();
+        body.style.backgroundImage = `url(${result.download_url})`;
+        author.textContent = `Author: ${result.author}`
+    } catch(error) {
+        console.error(error);
+    }
+}
+
+init();
